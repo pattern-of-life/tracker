76ccac7504063169ee5790fe5c2c84e434b4c058
Victor Benavente <vbenavente@hotmail.com>
2016-10-09 12:37:34 -0700

    updated requirements, urls for django registration, added registration templates

--- tracker/urls.py
+++ tracker/urls.py
@@ -13,7 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 from tracker.views import HomeView
 
@@ -21,4 +21,5 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', HomeView.as_view(
         template_name='tracker/home.html'), name='homepage'),
+    url(r'^accounts/', include('registration.backends.hmac.urls')),
 ]
