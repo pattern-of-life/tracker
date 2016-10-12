e722a19132578fe8ad9e449805ffdefc6e2f1170
Justin Lange <well1912@gmail.com>
2016-10-11 18:45:00 -0700

    Add STATIC_ROOT to settings.py

--- tracker/settings.py
+++ tracker/settings.py
@@ -137,9 +137,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,
-                 "static"),
+    os.path.join(BASE_DIR, "static"),
 ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 ACCOUNT_ACTIVATION_DAYS = 7
 
