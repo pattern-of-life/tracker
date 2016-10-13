cf37d748719efea0292731a5f82762ef885a1e89
Justin Lange <well1912@gmail.com>
2016-10-13 10:44:13 -0700

    Add LoginRequiredMixin to ProfileView

--- tracker_profile/tests.py
+++ tracker_profile/tests.py
@@ -63,6 +63,12 @@ class TrackerProfileViewTestCase(TestCase):
         url = reverse('detail_device', args=[self.device.pk])
         self.assertContains(self.response, url)
 
+    def test_profile_login_required(self):
+        """Test profile redirects to login."""
+        self.client.logout()
+        response = self.client.get(reverse('profile'))
+        self.assertEqual(response.status_code, 302)
+
 
 class TrackerProfileEditViewTestCase(TestCase):
     """Test profile editing."""
